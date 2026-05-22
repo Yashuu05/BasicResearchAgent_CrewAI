8b359fbdb156a0dc59f3219fa19910b5095da740
Yashuu05 <chillalyash2005@gmail.com>
2026-05-22 20:56:47 +0530

    Test: API key successfully loaded

--- tests/api_initialize.py
+++ tests/api_initialize.py
@@ -1,8 +1,15 @@
 from dotenv import load_dotenv
-from tavily import TavilyClient
+import os, sys
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+from src.crewai_project.logger import logging
 
 API = load_dotenv("TAVILY_API_KEY")
 if API is not None:
     print("API key loaded successfully")
+    logging.info("Tavily API Key successfully found.")
 else:
     print("API key not found. Check .env file")
+    logging.error("No API key Found for Tavily")
